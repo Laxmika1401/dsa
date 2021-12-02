c07814be2958cbb6a126d7e82bb3563af7902d76
Laxmika1401 <slaxmika@gmail.com>
2021-12-02 23:41:04 +0530

    initial commit

---
+++ Two_Sum.py
@@ -0,0 +1,11 @@
+https://leetcode.com/problems/two-sum/
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d={}
+        for i in range(len(nums)):
+            if nums[i] in d:
+                return [d[nums[i]],i]
+            else:
+                d[target-nums[i]]=i
