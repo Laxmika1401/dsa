2faf22011270a3102d6a12b8c37ac9186070481e
Laxmika1401 <slaxmika@gmail.com>
2021-12-05 23:19:34 +0530

    initial commit

---
+++ Single_Number.py
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans=0
+        for i in range(len(nums)):
+            ans^=nums[i]
+        return ans
