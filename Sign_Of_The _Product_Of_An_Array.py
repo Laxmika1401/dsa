5c2d52888826d867cf590d02705a9520ea09ef26
Laxmika1401 <slaxmika@gmail.com>
2021-12-05 23:11:00 +0530

    initial commit

---
+++ Sign_Of_The _Product_Of_An_Array.py
@@ -0,0 +1,15 @@
+
+https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        ans = 1
+        for i in range(len(nums)):
+            ans *= nums[i]
+        if ans > 0:
+            return 1
+        elif ans < 0:
+            return -1
+        else:
+            return 0
+        
